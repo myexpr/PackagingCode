78fe479282c617b17280acf81f73995e4777fc70
Krishna Kumar <krishna.kumar@thoughtfocus.com>
2020-06-25 13:39:18 +0530

    Commit Order, payment and product

---
+++ PackagingCode/PackagingCode/PackagingCode/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace packagingcode
+{
+    public class OrderItem
+    {
+        public IProduct product { get; set; }
+
+        public int Qty { get; set; }
+
+        public decimal total { get; set; }
+
+        public decimal discount { get; set; }
+
+    }
+}
---
+++ PackagingCode/PackagingCode/PackagingCode/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace packagingcode
+{
+    public class Payment
+    {
+        public bool Pay(Order order)
+        {
+            bool isPaid = true;
+
+            return isPaid;
+        }
+    }
+}
